4fe99840749ca9b933f5b56b2f8deaf6d501ba1a
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-09-26 16:44:54 -0400

    possibly fixed db not initializing error

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const bodyParser = require("body-parser")
 const db = require("./database/mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-app.use(bodyParser.json());
-app.use("/", require("./routes/index"));
 // require("./connection").main()
 
 db.initDb((err, mongodb) => {
     if (err) {
         console.log(err);
+    } else {
+        app.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST");
+            res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+        app.use(bodyParser.json());
+        app.use("/", require("./routes/index"));
+
+        app.listen(port, () => {
+            console.log(`Server is listning at port ${port}`);
+        })
     }
 });
-
-app.listen(port, () => {
-    console.log(`Server is listning at port ${port}`);
-})
