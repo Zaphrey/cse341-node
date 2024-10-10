4dc8188c201c55fc3a5d1764b3d17a3731f7bb4d
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-10-10 17:29:54 -0400

    removed original url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ db.initDb((err, mongodb) => {
         app.use(express.urlencoded({ extended: true }));
         app.use(bodyParser.json());
         app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "https://cse341-node-hjxg.onrender.com");
+            // res.setHeader("Access-Control-Allow-Origin", "https://cse341-node-hjxg.onrender.com/");
             res.setHeader("Access-Control-Allow-Origin", "https://cse341-contacts-frontend.netlify.app/");
             res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
             res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
