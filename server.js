05beac9b20ebd234cfaf3a0840557786c27e22cc
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-10-11 18:42:48 -0400

    i dont even know anymore

--- server.js
+++ server.js
@@ -10,12 +10,7 @@ db.initDb((err, mongodb) => {
     if (err) {
         console.log(err);
     } else {
-        app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-            res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
+        app.use(cors());
         app.use(express.urlencoded({ extended: true }));
         app.use(bodyParser.json());
         app.use("/", require("./routes/index"));
