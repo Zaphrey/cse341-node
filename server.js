f8b4e6e399e8d4b0c579949dcd4d0eb9175bc97a
Zachary Humphreys <zachary.humphreys32@gmail.com>
2024-10-11 18:40:21 -0400

    rearranged some things

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./database/mongodb");
+const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 3000;
 // require("./connection").main()
@@ -9,13 +10,13 @@ db.initDb((err, mongodb) => {
     if (err) {
         console.log(err);
     } else {
-        app.use(express.urlencoded({ extended: true }));
         app.use((req, res, next) => {
             res.setHeader("Access-Control-Allow-Origin", "*");
             res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
             res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
+        app.use(express.urlencoded({ extended: true }));
         app.use(bodyParser.json());
         app.use("/", require("./routes/index"));
 
